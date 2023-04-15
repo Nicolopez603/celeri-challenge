a57d5283d683aae66dcac9c11d4b2a8251ea1e27
Nicolas Lopez <81532585+Nicolopez603@users.noreply.github.com>
2023-04-15 20:17:15 -0300

    feat: finalizamos el test happy path

--- cypress/e2e/ui/caminoFeliz.cy.js
+++ cypress/e2e/ui/caminoFeliz.cy.js
@@ -20,13 +20,21 @@ describe("Test Suite del camino Feliz", () => {
 
     //Tercera Pagina
 
-    cy.get('input[name="id_number"]').type("42028075{enter}");
-    cy.get('input[name="tax_id_number"]').type("20-42028075-1{enter}");
+    cy.get('input[name="id_number"]').type(Cypress.env("user_dni"), {
+      log: false,
+    });
+    cy.get('input[name="tax_id_number"]').type(Cypress.env("user_cuit"), {
+      log: false,
+    });
     cy.get('input[name="first_name"]').type("Nicolas");
     cy.get('input[name="last_name"]').type("Lopez");
     cy.get('input[name="middle_name"]').type("Matias");
-    cy.get('input[name="phone"]').type("2213163549");
-    cy.get('input[name="email"]').type("nicomlopez3@gmail.com");
+    cy.get('input[name="phone"]').type(Cypress.env("user_telefono"), {
+      log: false,
+    });
+    cy.get('input[name="email"]').type(Cypress.env("user_mail"), {
+      log: false,
+    });
     cy.get(
       ":nth-child(1) > .MuiFormControl-root > .MuiFormGroup-root > :nth-child(2) > .MuiRadio-root > .PrivateSwitchBase-input"
     ).check();
@@ -109,5 +117,87 @@ describe("Test Suite del camino Feliz", () => {
       ':nth-child(2) > .flex > [aria-label="Agregar nuevo"] > .MuiButton-root'
     ).click();
     cy.location("pathname").should("contains", "human/third_party_accounts");
+
+    //CBU
+    cy.get(".MuiGrid-grid-md-8")
+      .click()
+      .type(Cypress.env("user_cbu"), { log: false });
+
+    //Banco
+    cy.get(".MuiDialogActions-root > .MuiButton-contained").click();
+    cy.get(
+      ":nth-child(3) > .flex > .MuiAutocomplete-root > .MuiFormControl-root > .MuiOutlinedInput-root"
+    )
+      .click()
+      .type("Naranja");
+    cy.get(
+      ".MuiAutocomplete-popper .MuiAutocomplete-listbox > :first-child"
+    ).click();
+
+    //Tipo de cuenta
+    cy.get(
+      ":nth-child(4) > .flex > .MuiAutocomplete-root > .MuiFormControl-root > .MuiOutlinedInput-root"
+    ).type("Billetera");
+    cy.get(
+      ".MuiAutocomplete-popper .MuiAutocomplete-listbox > :first-child"
+    ).click();
+
+    //Tipo de moneda
+    cy.get(
+      ":nth-child(5) > .flex > .MuiAutocomplete-root > .MuiFormControl-root > .MuiOutlinedInput-root"
+    ).type("Pesos Argentinos");
+    cy.get(
+      ".MuiAutocomplete-popper .MuiAutocomplete-listbox > :first-child"
+    ).click();
+
+    //Boton Continuar
+    cy.get(".justify-end > .MuiButton-contained").click();
+
+    //Quinta Pagina - Perfil del inversor
+    cy.get(":nth-child(3) > .flex > .MuiButton-root")
+      .contains("Completar")
+      .click();
+    cy.location("pathname").should("contains", "human/investor_profile");
+
+    //Experiencia en inversiones
+
+    cy.get(
+      ":nth-child(1) > .MuiFormControl-root > .MuiFormGroup-root > :nth-child(1) > .MuiRadio-root > .PrivateSwitchBase-input"
+    ).check();
+
+    //Grado de conocimiento
+
+    cy.get(
+      ":nth-child(2) > .MuiFormControl-root > .MuiFormGroup-root > :nth-child(1) > .MuiRadio-root > .PrivateSwitchBase-input"
+    ).check();
+    //Opero en bolsa
+    cy.get(
+      ":nth-child(3) > .MuiFormControl-root > .MuiFormGroup-root > :nth-child(1) > .MuiRadio-root > .PrivateSwitchBase-input"
+    ).check();
+
+    //Operaste en acciones?
+    cy.get(
+      ":nth-child(4) > .MuiFormControl-root > .MuiFormGroup-root > :nth-child(1) > .MuiRadio-root > .PrivateSwitchBase-input"
+    ).check();
+
+    //Boton contnuar
+    cy.get(".MuiButton-contained").click();
+
+    //Confirmar guardado
+    cy.get(".MuiDialogActions-root > .MuiButton-contained").click();
+
+    //Checkbox pantalla humana - Terminos y condiciones
+    cy.get(
+      ":nth-child(2) > .MuiFormControlLabel-root > .MuiCheckbox-root > .PrivateSwitchBase-input"
+    ).check();
+
+    //Checkbox pantalla humana - Politicas y Privacidad
+    cy.get(
+      ":nth-child(3) > .MuiFormControlLabel-root > .MuiCheckbox-root > .PrivateSwitchBase-input"
+    ).check();
+
+    //Boton - Abrir cuenta
+
+    cy.get(".css-fv3lde > .MuiButton-root").click();
   });
 });
