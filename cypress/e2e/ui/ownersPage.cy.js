916a7fd3fe1121d249234fc7b6f997927d8ad9c8
Nicolas Lopez <81532585+Nicolopez603@users.noreply.github.com>
2023-04-15 18:38:40 -0300

    feat: realizamos el camino feliz en titulares
    realizamos un camino feliz para la apertura de cuenta en titularidad

--- cypress/e2e/ui/ownersPage.cy.js
+++ cypress/e2e/ui/ownersPage.cy.js
@@ -1,20 +1,18 @@
 /// <reference types="cypress" />
 
-describe("Test Suit Pagina de Titulares", () => {
+describe("Test SuitE Pagina de Titulares", () => {
   beforeEach(function () {
     cy.visit("human/owners");
+    cy.location("protocol").should("contains", "https");
   });
 
-  it("should", () => {
+  it("Camino feliz de apertura de cuenta", () => {
     cy.get('input[name="id_number"]').type("42028075{enter}");
     cy.get('input[name="tax_id_number"]').type("20-42028075-1{enter}");
     cy.get('input[name="first_name"]').type("Nicolas");
     cy.get('input[name="last_name"]').type("Lopez");
     cy.get('input[name="middle_name"]').type("Matias");
     cy.get('input[name="phone"]').type("2213163549");
-    cy.get(
-      ":nth-child(10) > .flex > .MuiAutocomplete-root > .MuiFormControl-root > .MuiOutlinedInput-root"
-    ).type("Soltero");
     cy.get('input[name="email"]').type("nicomlopez3@gmail.com");
     cy.get(
       ":nth-child(1) > .MuiFormControl-root > .MuiFormGroup-root > :nth-child(2) > .MuiRadio-root > .PrivateSwitchBase-input"
@@ -29,19 +27,68 @@ describe("Test Suit Pagina de Titulares", () => {
     cy.get('input[name="addresses[0].zip_code"]').type("1900");
     cy.get('input[name="addresses[0].city"]').type("La Plata");
     cy.get('input[name="addresses[0].street"]').type("155");
-    cy.get(
-      ":nth-child(4) > :nth-child(2) > .MuiGrid-container > .MuiGrid-root > .flex > .MuiAutocomplete-root > .MuiFormControl-root > .MuiOutlinedInput-root"
-    ).type("Jubilado");
 
+    //Genero
     cy.get(
       ":nth-child(9) > .flex > .MuiAutocomplete-root > .MuiFormControl-root > .MuiOutlinedInput-root"
-    )
-      .click()
-      .first()
-      .click();
+    ).type("Masculino");
+
+    // Selecciona y hace clic en la primera opción del dropdown
+    cy.get(
+      ".MuiAutocomplete-popper .MuiAutocomplete-listbox > :first-child"
+    ).click();
+
+    //Estado civil
+    cy.get(
+      ":nth-child(10) > .flex > .MuiAutocomplete-root > .MuiFormControl-root > .MuiOutlinedInput-root"
+    ).type("Soltero");
 
     cy.get(
-      ":nth-child(10) > .flex > .MuiAutocomplete-root > .MuiFormControl-root > .MuiOutlinedInput-root > .MuiAutocomplete-endAdornment > .MuiButtonBase-root"
-    ).type("Masculino{enter}");
+      ".MuiAutocomplete-popper .MuiAutocomplete-listbox > :first-child"
+    ).click();
+
+    //Pais
+    cy.get(
+      ":nth-child(3) > .mb-16 > :nth-child(2) > .MuiGrid-container > :nth-child(1) > .flex > .MuiAutocomplete-root > .MuiFormControl-root > .MuiOutlinedInput-root"
+    ).type("Argentina");
+
+    cy.get(
+      ".MuiAutocomplete-popper .MuiAutocomplete-listbox > :first-child"
+    ).click();
+
+    //Provincia - Estado
+    cy.get(
+      ":nth-child(2) > .flex > .MuiAutocomplete-root > .MuiFormControl-root > .MuiOutlinedInput-root"
+    ).type("Buenos aires");
+
+    cy.get(
+      ".MuiAutocomplete-popper .MuiAutocomplete-listbox > :first-child"
+    ).click();
+
+    //Localidad
+
+    cy.get(
+      ":nth-child(3) > .mb-16 > :nth-child(2) > .MuiGrid-container > :nth-child(3) > .flex > .MuiAutocomplete-root > .MuiFormControl-root > .MuiOutlinedInput-root"
+    ).type("LA PLATA");
+
+    cy.get(
+      ".MuiAutocomplete-popper .MuiAutocomplete-listbox > :first-child"
+    ).click();
+
+    //Actividad economica
+    cy.get(
+      ":nth-child(4) > :nth-child(2) > .MuiGrid-container > .MuiGrid-root > .flex > .MuiAutocomplete-root > .MuiFormControl-root > .MuiOutlinedInput-root"
+    ).type("Venta al por mayor de azúcar");
+
+    cy.wait(500)
+
+    cy.get(
+      ".MuiAutocomplete-popper .MuiAutocomplete-listbox > :first-child"
+    ).click();
+
+    //Boton continuar
+
+    cy.get(".justify-end > .MuiButton-contained").click();
+    cy.location("pathname").should("contains", "/human");
   });
 });
